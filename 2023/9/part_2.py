d7c416e34b05c4d250461ebad4cb729e83538135
y-arjun-y <arjyadav7@gmail.com>
2023-12-16 11:06:01 +0400

    day 9

---
+++ 2023/9/part_2.py
@@ -0,0 +1,31 @@
+with open("2023/9/part_1_2_input.txt") as fobj:
+    sequences = fobj.readlines()
+    res = 0
+
+    for sequence in sequences:
+        total = None
+        line = list(map(int, sequence.split()))
+        sequence_breakdown = [line]
+
+        while total != 0:
+            lst = []
+
+            for i in range(1, len(line)):
+                lst.append(line[i] - line[i - 1])
+
+            line = lst
+            sequence_breakdown.append(line)
+            total = sum(lst)
+
+        sequence_breakdown = sequence_breakdown[::-1][1:]
+        sequence_breakdown[0] = [sequence_breakdown[0][0]] + sequence_breakdown[0]
+
+        for i in range(1, len(sequence_breakdown)):
+            sequence_breakdown[i] = [
+                sequence_breakdown[i][0] - sequence_breakdown[i - 1][0]
+            ] + sequence_breakdown[i]
+
+        print(sequence_breakdown)
+        res += sequence_breakdown[0][0]
+
+    print(res)
